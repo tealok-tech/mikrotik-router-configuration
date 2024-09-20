ec9b7158bab184e44e0b964d2db1ddbee33392a1
Eli Ribble <eli@theribbles.org>
2024-09-19 22:34:02 -0700

    Just show the interfaces and exit.
    I want to start building toward something, and looping forever won't
    help me much.

--- main.go
+++ main.go
@@ -6,26 +6,18 @@ import (
 	"fmt"
 	"log/slog"
 	"os"
-	"strings"
 	"time"
 
 	"github.com/tealok-tech/routeros/v3"
 )
 
 var (
-	debug      = flag.Bool("debug", false, "debug log level mode")
-	address    = flag.String("address", "192.168.88.1:8728", "Address")
-	username   = flag.String("username", "admin", "Username")
-	password   = flag.String("password", "admin", "Password")
-	properties = flag.String("properties", "name,rx-byte,tx-byte,rx-packet,tx-packet", "Properties")
-	timeout    = flag.Duration("timeout", 10*time.Second, "Connection timeout")
-	interval   = flag.Duration("interval", 1*time.Second, "Interval")
+	debug    = flag.Bool("debug", false, "debug log level mode")
+	address  = flag.String("address", "192.168.88.1:8728", "Address")
+	username = flag.String("username", "admin", "Username")
+	password = flag.String("password", "admin", "Password")
+	timeout  = flag.Duration("timeout", 10*time.Second, "Connection timeout")
 )
-
-func fatal(log *slog.Logger, message string, err error) {
-	log.Error(message, slog.Any("error", err))
-	os.Exit(2)
-}
 
 func main() {
 	var err error
@@ -58,21 +50,12 @@ func main() {
 
 	c.SetLogHandler(handler)
 
-	for {
-		var reply *routeros.Reply
-
-		if reply, err = c.Run("/interface/print", "?disabled=false", "?running=true", "=.proplist="+*properties); err != nil {
-			fatal(log, "could not run", err)
-		}
-
-		for _, re := range reply.Re {
-			for _, p := range strings.Split(*properties, ",") {
-				fmt.Print(re.Map[p], "\t")
-			}
-			fmt.Print("\n")
-		}
-		fmt.Print("\n")
-
-		time.Sleep(*interval)
+	// Get the number of interfaces
+	var reply *routeros.Reply
+	reply, err = c.Run("/interface/print", "=.proplist=name,tx-packet")
+	if err != nil {
+		log.Error("Failed to get interfaces", slog.Any("error", err))
+		os.Exit(3)
 	}
+	fmt.Println("Reply:", reply)
 }
